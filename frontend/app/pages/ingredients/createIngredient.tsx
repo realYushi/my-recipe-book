64467b70c51337c94319c1c3a896937b65e1f2fb
Yushi Cui <realYushi@gmail.com>
2025-04-16 10:00:15 +0000

    Update CreateIngredientPage layout: modify div to use flexbox for improved alignment and centering of the ingredient creation component.

--- frontend/app/pages/ingredients/createIngredient.tsx
+++ frontend/app/pages/ingredients/createIngredient.tsx
@@ -2,7 +2,7 @@ import CreateIngredient from "../../components/ingredient/createIngredient";
 
 export default function CreateIngredientPage() {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center h-screen">
             <CreateIngredient />
         </div>
     );

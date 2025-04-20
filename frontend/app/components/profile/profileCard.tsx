79ea50819c9da2ec7b7cae6e31a5317fe015535c
katsumi832 <Futak1192@gmail.com>
2025-04-21 11:02:30 +1200

    chore: update @radix-ui/react-avatar dependency to version 1.1.6 and add virtual module declaration for react-router server build types

--- frontend/app/components/profile/profileCard.tsx
+++ frontend/app/components/profile/profileCard.tsx
@@ -7,16 +7,12 @@ interface ProfileCardProps {
     avatar: string;
 }
 
-const ProfileCard: React.FC<ProfileCardProps> = ({ name, email, avatar }) => {
+const ProfileCard: React.FC<ProfileCardProps> = ({ avatar }) => {
     return (
         <div className="flex items-center gap-6">
             <EditableAvatar
                 avatar={avatar}
             />
-            <div>
-                <h2 className="text-2xl font-semibold">Name:<span className= "font-normal">{name}</span></h2>
-                <p className="text-gray-600">Email: <span className = "font-normal">{email}</span></p>
-            </div>
         </div>
     );
 }
--- frontend/app/pages/profilePage.tsx
+++ frontend/app/pages/profilePage.tsx
@@ -3,6 +3,8 @@ import ProfileCard from "~/components/profile/profileCard";
 import EditProfileButton from "~/components/profile/editProfileButton";
 import { userService } from "~/service/userService";
 import { authService } from "~/service/authService";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
 const ProfilePage: React.FC = () => {
     const [user, setUser] = useState({
@@ -57,16 +59,36 @@ const ProfilePage: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-white p-6">
-            <div className="flex items-center w-full max-w-3xl bg-grey-100 p-6 rounded-lg shadow-md">
-                <ProfileCard
-                    name={user.name}
-                    email={user.email}
-                    avatar={user.avatar}
-                />
-                <EditProfileButton
-                    onAvatarChange={handleAvatarChange}
-                    onNameChange={handleNameChange}
-                />
+            <div className="w-full max-w-md mx-auto">
+                <Card className="w-full">
+                    <CardHeader className="flex flex-col item-center gap-4">
+                        {/*<ProfileCard
+                            name={user.name}
+                            email={user.email}
+                            avatar={user.avatar}
+                        /> */}
+                        <CardTitle className="text-2x1 font-bold">Profile</CardTitle>
+                    </CardHeader>
+
+                    <CardContent className="grid w-full items-center gap-6">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Name</Label>
+                            <p className="text-lg font-semibold">{user.name}</p>
+                        </div>
+
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="email">Email</Label>
+                            <p className="text-lg font-semibold">{user.email}</p>
+                        </div>
+                    </CardContent>
+
+                    <CardFooter className="flex justify-end">
+                        <EditProfileButton
+                            onAvatarChange={handleAvatarChange}
+                            onNameChange={handleNameChange}
+                        />
+                    </CardFooter>
+                </Card>
             </div>
         </div>
     );
